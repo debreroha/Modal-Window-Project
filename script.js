dab8285811956c713a46a5b991cc71eb1f83b5e7
Getnet Alemye <124487134+debreroha@users.noreply.github.com>
2023-12-18 18:23:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
+// for loop to iterate
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
   btnCloseModal.addEventListener('click', closeModal);
