e5b916c3fae117dfc214faad8ce1bc9dd688a087
Getnet Alemye <124487134+debreroha@users.noreply.github.com>
2023-12-18 18:24:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   btnCloseModal.addEventListener('click', closeModal);
   overlay.addEventListener('click', closeModal);
 }
-
+// event listner for keydown
 document.addEventListener('keydown', function (e) {
 
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
