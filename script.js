debb9f07e04175d9636b774bc0fe7c2169b11dbf
Getnet Alemye <124487134+debreroha@users.noreply.github.com>
2024-01-18 18:53:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-// a logic to  open the modal
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
